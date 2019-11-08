9027adf977047d88d4b2c6ff1d1ac647d9b72631
SashoStoichkov <sashostoichkov@gmail.com>
2019-11-08 10:31:11 +0200

    08-11-2019 update

---
+++ 11-2019/08-11-2019.py
@@ -0,0 +1,34 @@
+from math import sqrt
+
+
+def is_prime(n):
+    if (n <= 1) or (n % 2 == 0):
+        return False
+    if (n == 2):
+        return True
+
+    i = 3
+    while i <= sqrt(n):
+        if n % i == 0:
+            return False
+        i = i + 2
+
+    return True
+
+
+def generate_prime_numbers():
+    number = 1
+
+    while True:
+        number += 1
+
+        if is_prime(number):
+            yield number
+
+
+gen = generate_prime_numbers()
+
+for i in gen:
+    if i > 100:
+        break
+    print(i)
